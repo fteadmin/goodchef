2baafa1595099ff28c958eaf2a451d0d8da29cc2
Ola <oyinkansola_o@yahoo.com>
2025-11-04 15:42:17 -0800

    removed teams

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import About from '@/components/sections/About';
 import Services from '@/components/sections/Services';
 import Chefs from '@/components/sections/Chefs';
 import Contact from '@/components/sections/Contact';
-import Teams from '@/components/sections/Teams';
+// import Teams from '@/components/sections/Teams';
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
       <About />
       <Services />
       <Chefs />
-      <Teams />
+      {/* <Teams /> */}
       <Contact />
     </>
   );
